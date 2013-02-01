780eac04e832db002cd0eec6231130991e414eb3
maxweld <dylan.maxwell@lightsource.ca>
2013-01-31 18:00:46 -0600

    Correct log messages in website configuration script.

--- config/20-website.py
+++ config/20-website.py
@@ -7,11 +7,11 @@ from twisted.web.server import Site
 from twisted.web.resource import Resource
 
 webroot = Resource()
-log.msg('website.py: Root Resource: %(r)s', r=webroot, logLevel=_TRACE)
+log.msg('website.py: Root: %(r)s', r=webroot, logLevel=_TRACE)
 
 website = Site(webroot)
-log.msg('website.py: Root Resource: %(r)s', r=webroot, logLevel=_TRACE)
+log.msg('website.py: Site: %(r)s', r=webroot, logLevel=_TRACE)
 
 webport = 8080
 reactor.listenTCP(webport, website)
-log.msg('website.py: Website listening on port: %(p)s', p=str(webport), logLevel=_DEBUG)
+log.msg('website.py: Site listening on port: %(p)s', p=str(webport), logLevel=_DEBUG)
