76e380f6f33716f222af19197bebaff61aae4373
maxweld <dylan.maxwell@lightsource.ca>
2013-02-08 15:08:09 -0600

    Update configuration script to log messages at INFO level.

--- config/25-webstatic.py
+++ config/25-webstatic.py
@@ -7,4 +7,4 @@ from twisted.web.static import File
 
 staticResource = File(os.path.join(csweb_home, "static"))
 webroot.putChild("static", staticResource)
-log.msg('webstatic.py: Resource added at "/static": %(r)s', r=staticResource, logLevel=_DEBUG)
+log.msg('webstatic.py: Resource added at "/static": %(r)s', r=staticResource, logLevel=_INFO)
