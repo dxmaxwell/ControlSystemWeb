1f30b8c5bae41dddbfce6cc571d0b9a507afc7b7
maxweld <dylan.maxwell@lightsource.ca>
2013-02-22 15:35:34 -0600

    Update Mail notifier configuration script to use SMTPAgent.

--- config/30-mailnotifier.py
+++ config/30-mailnotifier.py
@@ -3,9 +3,10 @@
 Initialize mail notifier.
 '''
 
+# from csweb.util.smtp import SMTPAgent
 # from csweb.notify.mail import MailNotifier
 
-# mailNotifier = MailNotifier(("smtp.host.com", 25), "From Name <from@host.com>")
+# mailNotifier = MailNotifier(SMTPAgent("smtp.host.com", 25), "From Name <from@host.com>")
 # log.msg('mailnotifier.py: MailNotifier: %(d)s', d=mailNotifier, logLevel=_INFO)
 
 # mailNotifier.register("epics:SR2026X:Status", "Real Name <email@host.com>")
